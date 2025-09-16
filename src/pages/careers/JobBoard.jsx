0eba9433719ab4f9ed094d4e3501b5529101656a
Kishore270426 <kishorek2704@gmail.com>
2025-09-16 16:16:53 +0530

    updated url for careers

--- src/pages/careers/JobBoard.jsx
+++ src/pages/careers/JobBoard.jsx
@@ -23,7 +23,7 @@ const JobBoard = () => {
     const fetchJobs = async () => {
       try {        
         await new Promise(resolve => setTimeout(resolve, 800));        
-        const response = await fetch('http://127.0.0.1:8080/api/careers/jobDetails');        
+        const response = await fetch('http://97.74.87.167/api/careers/jobDetails');        
         if (!response.ok) {
           throw new Error(`Server responded with status: ${response.status}`);
         }
