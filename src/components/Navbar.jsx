96af891bb9b3e3c3d052732ea0ba70ef1537c8af
Kishore270426 <kishorek2704@gmail.com>
2025-09-17 12:39:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
             className="flex items-center cursor-pointer"
             onClick={() => navigate("/")}
           >
-            <img src="src/assets/logo.png" alt="Logo" className="h-15 w-auto ml-4" />
+            <img src="home/vpearl/apps/Vpearl_Website_FrontEnd/src/assets/logo.png" alt="Logo" className="h-15 w-auto ml-4" />
             <span className="text-xl font-semibold text-black ml-2 tracking-wide">
               <span className="text-3xl">V</span>Pearl<span className="text-3xl"></span>Solutions
             </span>
